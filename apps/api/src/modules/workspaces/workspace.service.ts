50587433db393bd3f3fcb63d062af1cf0a4495ae
Akhilesh Singh <singhakhilesh19468@gmail.com>
2026-04-06 00:20:38 +0530

    feat(workspaces): implement workspace creation service with transaction and owner assignment

---
+++ apps/api/src/modules/workspaces/workspace.service.ts
@@ -0,0 +1,36 @@
+import { prisma } from "@/lib/prisma";
+
+type createWorkspaceInput={
+    name: string;
+    slug: string;
+}
+
+export const creteWorkspace =async (userId: string, data: createWorkspaceInput) => {
+    try {
+        const workspace = await prisma.$transaction(async (tx)=>{
+            const createWorkspace = await tx.workspace.create({
+                data: {
+                    name: data.name,
+                    slug: data.slug
+                }
+            })
+
+            await tx.workspaceMember.create({
+                data: {
+                    userId,
+                    workspaceId: createWorkspace.id,
+                    role: "OWNER"
+                }
+            })
+            return createWorkspace;
+        })
+        return workspace;
+
+    } catch (error: any) {
+        if(error.code === "P2002"){
+            throw new Error("Workspace slug already exists");
+        }
+
+        throw error;
+    }
+}
