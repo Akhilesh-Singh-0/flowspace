ea0c5cd97adac74b34c727a4298b0bd7be177c21
Akhilesh Singh <singhakhilesh19468@gmail.com>
2026-04-07 11:06:48 +0530

    feat(auth): add requireAuth middleware with Clerk token verification

---
+++ apps/api/src/middleware/requireAuth.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "@clerk/backend";
+
+export const authMiddleware =async (req: Request, res: Response,  next: NextFunction) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer")){
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized"
+            })
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        const verified = await verifyToken(token, {
+            secretKey: process.env.CLERK_SECRET_KEY!
+        });
+
+        if(!verified){
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized"
+            })
+        }
+
+        req.user = { userId: verified.sub };
+
+        next();
+
+    } catch (error) {
+        next(error);
+    }
+}
