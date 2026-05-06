8b1c116e58657b73f557694cbdf4b740b09e9b58
Akhilesh Singh <singhakhilesh19468@gmail.com>
2026-05-06 11:40:03 +0530

    fix(web): remove unnecessary next.config.mjs overrides

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,15 +1,3 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    externalDir: true,
-  },
-  outputFileTracingRoot: path.resolve(__dirname, "../../"),
-};
-
+const nextConfig = {};
 export default nextConfig;
